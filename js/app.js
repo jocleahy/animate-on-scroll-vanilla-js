0e9a20ad7fa84376653c64385039d798bb5e9563
jocleahy <leahyjuliannz@gmail.com>
2020-04-03 21:24:08 +1300

    add debounce function to limit scroll event

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait = 20, immediate) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        }, wait);
+        if (immediate && !timeout) func.apply(context, args);
+    };
+}
+
+
+
+
 // select all images with the animate class
 const aniImgs = document.querySelectorAll('.can-animate');
 
@@ -27,5 +47,5 @@ function scrollCheck(e) {
         }
     })
 }
-window.addEventListener('scroll', scrollCheck);
+window.addEventListener('scroll', debounce(scrollCheck));
 
